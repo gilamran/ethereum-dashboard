5494ae8f2f79ef254251db2e0ef5af297eee7fb9
Gil Amran <gilamran@gmail.com>
2018-02-05 17:50:27 +0200

    removed react bootstrap

--- src/client/components/Home/Home.tsx
+++ src/client/components/Home/Home.tsx
@@ -1,15 +1,14 @@
 import * as React from 'react';
-import { Jumbotron } from 'react-bootstrap';
 import { TransactionsBox } from '../TransactionsBox/TransactionsBox';
 import { BlocksBox } from '../BlocksBox/BlocksBox';
 
 const css = require('./Home.css');
 
 export const Home = () => (
-  <Jumbotron>
+  <div>
     <h1>Ethereum Dashboard</h1>
     <p>#1 Ethereume dashboard.</p>
     <TransactionsBox />
     <BlocksBox />
-  </Jumbotron>
+  </div>
 );
